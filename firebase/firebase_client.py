887fc87ddf9accbfa60d54574739a0799380d994
Dmitry Ivanov <thedivanov@gmail.com>
2024-09-24 18:12:57 +0200

    apns priority

--- firebase/firebase_client.py
+++ firebase/firebase_client.py
@@ -41,8 +41,8 @@ class FirebaseClient:
                     aps=messaging.Aps(content_available=True)  # Устанавливаем флаг сайлент-пуша
                 ),
                 headers={
-                'apns-priority': '10',  # Высокий приоритет доставки для iOS
-            }
+                    'apns-priority': '10',  # Высокий приоритет доставки для iOS
+                }
             ),
             # TODO: мб понадобится
             # android=messaging.AndroidConfig(
