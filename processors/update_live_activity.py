9e6dad1c83f9a3ca482b25f0db95a608854de9ce
Dmitry Ivanov <thedivanov@gmail.com>
2024-10-03 17:34:46 +0200

    fix update live activities

--- processors/update_live_activity.py
+++ processors/update_live_activity.py
@@ -11,18 +11,6 @@ from processors.base import base_process, silent_base_process
 from rabbitmq.rabbitmq import RabbitMQProducer
 
 
-class DataObject:
-    def __init__(self, data):
-        self.__dict__.update(data)
-
-    def __getitem__(self, key):
-        return getattr(self, key, None)
-
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
-
-
-
 async def update_live_activity_process(message: aio_pika.IncomingMessage):
     async with message.process():
         print("Received message from queue:", SENT_LIVE_ACTIVITY_UPDATE_PUSH_NAME, ' message:', message.body)
@@ -31,7 +19,7 @@ async def update_live_activity_process(message: aio_pika.IncomingMessage):
             token = data['push_token']
             apphud_user_id = data.get('apphud_user_id')
 
-            payload = DataObject(data)
+            payload = data.copy()
 
             rabbitmq_client = RabbitMQProducer()
 
@@ -39,6 +27,7 @@ async def update_live_activity_process(message: aio_pika.IncomingMessage):
                                       apphud_user_id)
         except Exception as e:
             print(f"Error processing message: {e}")
+
 
 
 # TODO: we need this???
